1bdcc77c4674f1712e6ae0b34f0edacb6690a2fa
akshatphumbhra <43840267+akshatphumbhra@users.noreply.github.com>
2020-09-18 02:59:22 +0530

    Completed first exercise

--- index.js
+++ index.js
@@ -11,8 +11,19 @@ numbers together and return the sum
 
 Step 3. Call the function
 
-For extra credit, create a conditional that executes a message if the sum is over 10 or less than 100 
+For extra credit, create a conditional that executes a message if the sum is over 10 or less than 100
 
 ********************/
 
 // Your Solution:
+
+function add(num1, num2){
+  var sum = num1 + num2;
+  //I'm unsure as to what the conditional is supposed to look like, so this is my best guess
+  if(sum > 10 && sum < 100){
+    alert("The sum is greater than 10 but less than 100");
+  }
+  return sum;
+}
+
+add(1, 12);
